cb0a7a63d07a5e9ecae671b56e5ed5c6c1a77a24
Jacob Visser <jacobvisser123@hotmail.com>
2022-11-28 12:00:52 +0100

    Initial commit om alles op git te zetten.

---
+++ src/main/java/nl/miwgroningen/ch10/jacob/project_leerlingvolgsysteem/ProjectLeerlingvolgsysteemApplication.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.ch10.jacob.project_leerlingvolgsysteem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLeerlingvolgsysteemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLeerlingvolgsysteemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwgroningen/ch10/jacob/project_leerlingvolgsysteem/ProjectLeerlingvolgsysteemApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.ch10.jacob.project_leerlingvolgsysteem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLeerlingvolgsysteemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
