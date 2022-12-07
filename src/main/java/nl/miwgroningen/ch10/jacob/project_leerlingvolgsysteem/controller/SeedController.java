7b820d24cdfbfef2e76431776940cef557fe9231
Jacob Visser <jacobvisser123@hotmail.com>
2022-12-07 11:33:09 +0100

    seeder uitgebreid

--- src/main/java/nl/miwgroningen/ch10/jacob/project_leerlingvolgsysteem/controller/SeedController.java
+++ src/main/java/nl/miwgroningen/ch10/jacob/project_leerlingvolgsysteem/controller/SeedController.java
@@ -1,14 +1,12 @@
 package nl.miwgroningen.ch10.jacob.project_leerlingvolgsysteem.controller;
 
-import nl.miwgroningen.ch10.jacob.project_leerlingvolgsysteem.model.Assignment;
-import nl.miwgroningen.ch10.jacob.project_leerlingvolgsysteem.model.Course;
-import nl.miwgroningen.ch10.jacob.project_leerlingvolgsysteem.model.Student;
-import nl.miwgroningen.ch10.jacob.project_leerlingvolgsysteem.repository.AssignmentRepository;
-import nl.miwgroningen.ch10.jacob.project_leerlingvolgsysteem.repository.CourseRepository;
-import nl.miwgroningen.ch10.jacob.project_leerlingvolgsysteem.repository.StudentRepository;
+import nl.miwgroningen.ch10.jacob.project_leerlingvolgsysteem.model.*;
+import nl.miwgroningen.ch10.jacob.project_leerlingvolgsysteem.repository.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -20,10 +18,18 @@ private final StudentRepository studentRepository;
 
 private final AssignmentRepository assignmentRepository;
 
-    public SeedController(CourseRepository courseRepository, StudentRepository studentRepository, AssignmentRepository assignmentRepository) {
+private final FeedbackRepository feedbackRepository;
+
+private final SubmittedVersionRepository submittedVersionRepository;
+
+private final AssessmentRepository assessmentRepository;
+    public SeedController(CourseRepository courseRepository, StudentRepository studentRepository, AssignmentRepository assignmentRepository, FeedbackRepository feedbackRepository, SubmittedVersionRepository submittedVersionRepository, AssessmentRepository assessmentRepository) {
         this.courseRepository = courseRepository;
         this.studentRepository = studentRepository;
         this.assignmentRepository = assignmentRepository;
+        this.feedbackRepository = feedbackRepository;
+        this.submittedVersionRepository = submittedVersionRepository;
+        this.assessmentRepository = assessmentRepository;
     }
 
 @GetMapping("/seed")
@@ -43,26 +49,34 @@ private final AssignmentRepository assignmentRepository;
     courses.add(OOP);
     courses.add(programming);
 
-    Student jacob = new Student();
-    jacob.setFirstName("Dummy");
-    jacob.setInFixName("tussen");
-    jacob.setLastName("Dommy");
-    jacob.setGithubAccount("Obiduck11");
-    jacob.setCourses(courses);
+    Student student1 = new Student();
+    student1.setFirstName("Wytse");
+    student1.setInFixName("");
+    student1.setLastName("Boiten");
+    student1.setGithubAccount("Harrinator");
+    student1.setCourses(courses);
 
-    Student dommy = new Student();
-    dommy.setFirstName("dommy2");
-    dommy.setInFixName("van");
-    dommy.setLastName("test");
-    dommy.setGithubAccount("narnja");
-    dommy.setCourses(courses);
+    Student student2 = new Student();
+    student2.setFirstName("Hasna");
+    student2.setInFixName("van");
+    student2.setLastName("Cook");
+    student2.setGithubAccount("Cookbob");
+    student2.setCourses(courses);
+
+    Student student3 = new Student();
+    student3.setFirstName("Stina");
+    student3.setInFixName("van der");
+    student3.setLastName("Snyder");
+    student3.setGithubAccount("EssEmOh");
+    student3.setCourses(courses);
 
     Set<Student> students = new HashSet<>();
-    students.add(jacob);
-    students.add(dommy);
+    Collections.addAll(students, student1, student2, student3);
 
-    studentRepository.save(jacob);
-    studentRepository.save(dommy);
+
+    studentRepository.save(student1);
+    studentRepository.save(student2);
+    studentRepository.save(student3);
 
     Assignment verslag = new Assignment();
     verslag.setTitle("reflectie");
@@ -70,9 +84,79 @@ private final AssignmentRepository assignmentRepository;
     verslag.setDescription("Dit verslag is bedoeld om na te denken over de zin en onzin van programming");
     verslag.setSerialNumber(1);
 
-    assignmentRepository.save(verslag);
+    Assignment werkstuk = new Assignment();
+    werkstuk.setTitle("werkstuk");
+    werkstuk.setCourse(programming);
+    werkstuk.setDescription("mooi werkstuk over programming maken");
+    werkstuk.setSerialNumber(2);
 
+    Assignment practicum = new Assignment();
+    practicum.setTitle("werkstuk");
+    practicum.setCourse(programming);
+    practicum.setDescription("Oefenen met variabelen in Java.");
+    practicum.setSerialNumber(3);
+
+
+
+    assignmentRepository.save(verslag);
+    assignmentRepository.save(werkstuk);
+    assignmentRepository.save(practicum);
+
+    Set<Assignment> assignments = new HashSet<>();
+    Collections.addAll(assignments, verslag,werkstuk,practicum);
+
+
+    programming.setAssignments(assignments);
     OOP.setStudents(students);
+
+    Feedback feedback1 = new Feedback();
+    feedback1.setTitle("SABOR");
+    feedback1.setContent("geen spaties");
+
+    Feedback feedback2 = new Feedback();
+    feedback2.setTitle("Indentie");
+    feedback2.setContent("te weinig indentie, zo is er geen overzicht");
+
+    Feedback feedback3 = new Feedback();
+    feedback3.setTitle("Magic numbers");
+    feedback3.setContent("onverklaarbare cijfers zijn niet goed te lezen voor je collega's");
+
+    Set<Feedback> feedbacks = new HashSet<>();
+    Collections.addAll(feedbacks, feedback1, feedback2, feedback3);
+
+    feedbackRepository.saveAll(feedbacks);
+
+    SubmittedVersion submittedVersion1 = new SubmittedVersion();
+    submittedVersion1.setAssignment(verslag);
+    submittedVersion1.setDateSubmitted(LocalDate.of(2022,9,11));
+    submittedVersion1.setStudent(student1);
+
+    SubmittedVersion submittedVersion2 = new SubmittedVersion();
+    submittedVersion2.setAssignment(practicum);
+    submittedVersion2.setDateSubmitted(LocalDate.of(2022,5,3));
+    submittedVersion2.setStudent(student2);
+
+
+    submittedVersionRepository.save(submittedVersion1);
+    submittedVersionRepository.save(submittedVersion2);
+
+
+    Assessment assessment1 = new Assessment();
+    assessment1.setPass(false);
+    assessment1.setFeedbacks(feedbacks);
+    assessment1.setSpecifiekeFeedback("goed gedaan");
+    assessment1.setSubmittedVersion(submittedVersion1);
+
+    Assessment assessment2 = new Assessment();
+    assessment2.setPass(true);
+    assessment2.setFeedbacks(feedbacks);
+    assessment2.setSpecifiekeFeedback("minder goed gedaan");
+    assessment2.setSubmittedVersion(submittedVersion2);
+
+    assessmentRepository.save(assessment1);
+    assessmentRepository.save(assessment2);
+
+
 
 
     return "redirect:/students/all";
