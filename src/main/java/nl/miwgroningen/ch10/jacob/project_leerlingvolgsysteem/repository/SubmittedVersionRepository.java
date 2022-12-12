06acf916ebe486cba7487c0875f41105480e73ae
Robbin <robbindrent@gmail.com>
2022-12-12 11:34:21 +0100

    SubmittedVersion georderd op datum toegevoegd

--- src/main/java/nl/miwgroningen/ch10/jacob/project_leerlingvolgsysteem/repository/SubmittedVersionRepository.java
+++ src/main/java/nl/miwgroningen/ch10/jacob/project_leerlingvolgsysteem/repository/SubmittedVersionRepository.java
@@ -3,10 +3,14 @@ package nl.miwgroningen.ch10.jacob.project_leerlingvolgsysteem.repository;
 import nl.miwgroningen.ch10.jacob.project_leerlingvolgsysteem.model.SubmittedVersion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * @author Robbin Drent <r.v.drent@st.hanze.nl>
  * <p>
  * Dit is wat het programma doet.
  */
 public interface SubmittedVersionRepository extends JpaRepository<SubmittedVersion, Long> {
+
+    List<SubmittedVersion> findByOrderByDateSubmittedDesc();
 }
