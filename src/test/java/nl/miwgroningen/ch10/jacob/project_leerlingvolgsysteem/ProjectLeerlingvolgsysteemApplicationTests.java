b1c7c2c4e11b6735c7470c74d47af6c87ae41627
MaaikeFeenstra <maaikefeenstra@outlook.com>
2022-12-14 09:56:25 +0100

    Oude naamtesten die niet werkten weggehaald

--- src/test/java/nl/miwgroningen/ch10/jacob/project_leerlingvolgsysteem/ProjectLeerlingvolgsysteemApplicationTests.java
+++ src/test/java/nl/miwgroningen/ch10/jacob/project_leerlingvolgsysteem/ProjectLeerlingvolgsysteemApplicationTests.java
@@ -11,35 +11,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest (classes = ProjectLeerlingvolgsysteemApplicationTests.class)
 class ProjectLeerlingvolgsysteemApplicationTests {
 
-	@Test
-	@DisplayName("DisplayNameWithoutInfixName")
-	void StudentDisplayNameTest() {
-		Student student = new Student();
-		String expectedStudentName = "Robbin Drent";
 
-		String studentName = student.getDisplayName();
-
-		assertEquals(expectedStudentName, studentName);
-	}
-
-	@Test
-	@DisplayName("DisplayNameWithInfixName")
-	void StudentDisplayNameWithInfixTest() {
-		Student student = new Student();
-		String expectedStudentName = "Henk van Dijk";
-
-		String studentName = student.getDisplayName();
-
-		assertEquals(expectedStudentName, studentName);
-	}
 	@Test
 	@DisplayName("DisplayName")
 	void DisplayNameTest() {
-		Student student = new Student();
+		Student student = new Student("Henk", "van", "Dijk");
 		String expectedStudentName = "Henk van Dijk";
-		student.setFirstName("Henk");
-		student.setInFixName("van");
-		student.setLastName("Dijk");
+		String studentName = student.getDisplayName();
+		assertEquals(expectedStudentName, studentName);
+	}
+	@Test
+	@DisplayName("DisplayNameWithoutPrefix")
+	void DisplayNameTestWithoutPrefix() {
+		Student student = new Student("Kees","Test");
+		String expectedStudentName = "Kees Test";
+
 		String studentName = student.getDisplayName();
 		assertEquals(expectedStudentName, studentName);
 	}
